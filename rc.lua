a4a8a7bc21399f78cb3f298adacd7efa91c2c0ce
ben <>
2019-08-09 14:05:09 +0200

    fix quake terminal

--- rc.lua
+++ rc.lua
@@ -15,7 +15,7 @@ local naughty = require("naughty")
 local menubar = require("menubar")
 local hotkeys_popup = require("awful.hotkeys_popup")
 -- Dropdown Terminal
---local dropdownterm = require("scratchdrop")
+local dropdownterm = require("scratchdrop")
 -- Enable hotkeys help widget for VIM and other apps
 -- when client with a matching name is opened:
 require("awful.hotkeys_popup.keys")
